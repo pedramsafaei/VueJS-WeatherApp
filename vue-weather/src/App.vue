4736cad61796732ac93ac5e7fde8450664ed6b7b
ps <ps@pop-os.localdomain>
2020-04-03 01:15:49 -0700

    search box and skeleton of the program added

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -1,11 +1,63 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      api_key: "f3fbefcd7e59c921ee9721b5795bff5a"
+    };
+  }
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Courier New", Courier, monospace;
+}
+#app {
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
