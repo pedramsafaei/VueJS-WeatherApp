49197d1fd873ba4be4d513773e84854f9e7089a9
ps <ps@pop-os.localdomain>
2020-04-04 00:38:12 -0700

    search box improved

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -58,5 +58,13 @@ main {
   outline: none;
   background: none;
   background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 15px 0px 15px;
+  transition: 0.4s;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 15px 0px 15px 0px;
 }
 </style>
